30fc7f43529caaf7b999358fab0e4a5f679a708d
VanHieuVu <vanhieuvu01@gmail.com>
2025-07-20 08:34:35 +0200

    adding landing page for the app

--- apps/rep_app/tests.py
+++ apps/rep_app/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+
+def test_landing_page():
+    client = Client()
+    response = client.get('/')
+    assert response.status_code == 200
--- rep_project/urls.py
+++ rep_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.rep_app.views import landing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing, name='landing'),
 ]
