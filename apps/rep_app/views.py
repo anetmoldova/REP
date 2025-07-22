37cd7b4144a32aae54158b7aa479d7facbd799e2
VanHieuVu <vanhieuvu01@gmail.com>
2025-07-22 21:55:39 +0200

    update the landing page and add login and signup pages

--- apps/rep_app/views.py
+++ apps/rep_app/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def landing(request):
-    return render(request, 'rep_app/landing.html')
+    image_filename = 'rep_app/preview_landing_page_v2.png'  # You can change this dynamically
+    return render(request, 'rep_app/landing.html', {'landing_image': image_filename})
+
+def signup(request):
+    return render(request, 'rep_app/signup.html')
+
+def login_page(request):
+    return render(request, 'rep_app/login.html')
--- rep_project/urls.py
+++ rep_project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.rep_app.views import landing
+from apps.rep_app.views import landing, signup, login_page
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing, name='landing'),
+    path('signup/', signup, name='signup'),
+    path('login/', login_page, name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
